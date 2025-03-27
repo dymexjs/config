80c0096a613f9aed41519f1162a074ffdb38ea48
João Parreira <n3okill@hotmail.com>
2025-03-27 12:15:41 +0000

    chore: added type coercion for number boolean and date

--- src/configuration-source.ts
+++ src/configuration-source.ts
@@ -1,23 +1,21 @@
-import { type TConfiguration } from "./types/configuration.ts";
-import { expand } from "./helpers.ts";
-import { type ConfigSourceOptions } from "./types/config-source-options.ts";
-import { type IConfiguration } from "./types/configuration.ts";
-import { type ValidatorFunc } from "./types/validator-func.type.ts";
+import type { TConfiguration } from "./types/configuration.ts";
+import { coerce, interpolate, isObject, resolveEscapeSequence } from "./helpers.ts";
+import type { ConfigSourceOptions } from "./types/config-source-options.ts";
+import type { IConfiguration } from "./types/configuration.ts";
+import type { ValidatorFunc } from "./types/validator-func.type.ts";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export abstract class ConfigurationSource<T = any> {
   protected dataOlder: T;
   protected expandVariables = true;
+  protected coerce = true;
   protected validation: ValidatorFunc = (config) => Promise.resolve(config);
 
   constructor(dataOlder: T, options: ConfigSourceOptions = {}) {
     this.dataOlder = dataOlder;
-    if (options.expandVariables) {
-      this.expandVariables = options.expandVariables as boolean;
-    }
-    if (options.validation) {
-      this.validation = options.validation;
-    }
+    this.expandVariables = options.expandVariables ?? true;
+    this.coerce = options.coerce ?? true;
+    this.validation = options.validation ?? ((config) => Promise.resolve(config));
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -25,8 +23,25 @@ export abstract class ConfigurationSource<T = any> {
   abstract build(): Promise<TConfiguration>;
   async postBuild(config: TConfiguration, configuration: IConfiguration): Promise<TConfiguration> {
     if (this.expandVariables) {
-      expand(config, configuration);
+      this.expand(config, configuration);
     }
     return this.validation(config) as Promise<TConfiguration>;
+  }
+
+  expand(obj: TConfiguration, config: IConfiguration): TConfiguration {
+    for (const [key, value] of Object.entries(obj)) {
+      if (typeof value === "string") {
+        let valueAux = interpolate(value, obj, config);
+        valueAux = resolveEscapeSequence(valueAux);
+        Object.defineProperty(obj, key, {
+          value: this.coerce && typeof valueAux === "string" ? coerce(valueAux) : valueAux,
+          enumerable: true,
+          writable: true,
+        });
+      } else if (isObject(value)) {
+        this.expand(value, config);
+      }
+    }
+    return obj;
   }
 }
--- src/helpers.ts
+++ src/helpers.ts
@@ -26,22 +26,147 @@ export function isObject(arg: unknown): arg is object {
   );
 }
 
-//#region Expand env variables
+//#region Coerce
 
-export function expand(obj: TConfiguration, config: IConfiguration): TConfiguration {
-  for (const [key, value] of Object.entries(obj)) {
-    if (typeof value === "string") {
-      let valueAux = interpolate(value, obj, config);
-      valueAux = resolveEscapeSequence(valueAux);
-      Object.defineProperty(obj, key, { value: valueAux, enumerable: true, writable: true });
-    } else if (isObject(value)) {
-      expand(value, config);
+/**
+ * Attempts to convert a string value to a number, boolean, or Date.
+ * 
+ * @param value - The string value to be coerced.
+ * @returns The coerced value as a number, boolean, Date, or the original string if no coercion is possible.
+ */
+export function coerce(value: string): string | number | boolean | Date {
+  const coercers = [tryCoerceNumber, tryCoerceBoolean, tryCoerceDate];
+  for (const coercer of coercers) {
+    const [coerce, result] = coercer(value);
+    if (coerce) {
+      return result;
     }
   }
-  return obj;
+  return value;
 }
 
-function interpolate(value: string, obj: TConfiguration, configuration: IConfiguration): string {
+  /**
+   * Attempts to coerce a string value to a boolean.
+   * 
+   * @param value - The string value to be coerced.
+   * @returns A boolean indicating whether the coercion was successful, and the coerced value as a boolean, or the original string if no coercion is possible.
+   */
+function tryCoerceBoolean(value: string): [boolean, boolean | string] {
+  const booleanValue = value.toLowerCase();
+  if (booleanValue === "true" || booleanValue === "false") {
+    return [true, booleanValue === "true"];
+  }
+  return [false, value];
+}
+
+/**
+ * Attempts to coerce a string value to a number.
+ * 
+ * @param value - The string value to be coerced.
+ * @returns A boolean indicating whether the coercion was successful, and the coerced value as a number, or the original string if no coercion is possible.
+ */
+function tryCoerceNumber(value: string): [boolean, number | string] {
+  // eslint-disable-next-line security/detect-unsafe-regex
+  const matches = value.match(/^\s*[+-]?(?:(?:\d+(?:\.\d*)?)|(?:\.\d+))(?:e([+-]?\d+))?\s*$/i);
+  if (!matches) {
+    return [false, value];
+  }
+  const result = parseFloat(value);
+
+  if (result === 0) {
+    return [true, 0]; // -0
+  }
+
+  if (value.match(/e/i)) {
+    if (extractSignificantDigits(value) !== extractSignificantDigits(String(result))) {
+      return [false, value];
+    }
+  } else {
+    const string = result.toString();
+    if (string.match(/e/i)) {
+      return [true, result];
+    }
+
+    if (string !== normalizeDecimal(value)) {
+      return [false, value];
+    }
+  }
+
+  return [true, result];
+}
+
+/**
+ * Extracts the significant digits from a string representation of a number.
+ * 
+ * @param value - The string value from which to extract significant digits.
+ * @returns The string of significant digits extracted from the input value.
+ */
+function extractSignificantDigits(value: string) {
+  return (
+    value
+      .replace(/[eE][+-]?\d+$/, "")
+      .replace(/\./, "")
+      .replace(/0+$/, "")
+      // eslint-disable-next-line security/detect-unsafe-regex
+      .replace(/^[+-]?(0*)?/, "")
+  );
+}
+
+/**
+ * Normalizes a decimal string by removing unnecessary decimal points and leading/trailing zeros.
+ * 
+ * @param str - The string value to be normalized.
+ * @returns The normalized string value.
+ */
+function normalizeDecimal(str: string) {
+  str = str
+    // Remove leading plus signs
+    .replace(/^\+/, "")
+    // Remove trailing zeros if there is a decimal point and unecessary decimal points
+    .replace(/\.0*$/, "")
+    // Add a integer 0 if the numbers starts with a decimal point
+    .replace(/^(-?)\.([^.]*)$/, "$10.$2")
+    // Remove leading zeros
+    .replace(/^(-?)0+([0-9])/, "$1$2");
+
+  if (str.includes(".") && str.endsWith("0")) {
+    str = str.replace(/0+$/, "");
+  }
+
+  if (str === "-0") {
+    return "0";
+  }
+
+  return str;
+}
+
+/**
+ * Attempts to coerce a string value to a Date.
+ * 
+ * @param value - The string value to be coerced.
+ * @returns A boolean indicating whether the coercion was successful, and the coerced value as a Date, or the original string if no coercion is possible.
+ */
+function tryCoerceDate(value: string): [boolean, Date | string] {
+  const date = new Date(value);
+  if (!isNaN(date.getTime())) {
+    return [true, date];
+  }
+  return [false, value];
+}
+
+//#endregion
+
+//#region Expand env variables
+
+/**
+ * Interpolates environment variables in a string value.
+ * 
+ * @param value - The string value containing environment variables to be interpolated.
+ * @param obj - The configuration object containing the configuration values.
+ * @param configuration - The configuration object containing the configuration values.
+ * @returns The interpolated string value.
+ */
+export function interpolate(value: string, obj: TConfiguration, configuration: IConfiguration): string {
   return value.replace(DOTENV_SUBSTITUTION_REGEX, (match, key, defaultValue) => {
     const configurationValue = configuration.get(key);
     if (!isUndefined(configurationValue)) {
@@ -57,6 +182,8 @@ function interpolate(value: string, obj: TConfiguration, configuration: IConfigu
       // avoid recursion from EXPAND_SELF=$EXPAND_SELF
       if (objKey === value) {
         return value;
+      } else if (typeof objKey !== "string") {
+        return objKey.toString();
       } else {
         return interpolate(objKey, obj, configuration);
       }
@@ -72,12 +199,24 @@ function interpolate(value: string, obj: TConfiguration, configuration: IConfigu
   });
 }
 
-function resolveEscapeSequence(value: string): string {
+/**
+ * Resolves escape sequences in a string value.
+ * 
+ * @param value - The string value containing escape sequences to be resolved.
+ * @returns The resolved string value.
+ */
+export function resolveEscapeSequence(value: string): string {
   return value.replace(/\\\$/g, "$");
 }
 
 //#endregion
 
+/**
+ * Recursively merges properties from the source object into the target object.
+ * 
+ * @param target - The target object to which properties will be merged.
+ * @param source - The source object from which properties will be merged.
+ */
 export function deepMixIn(target: TConfiguration, source: TConfiguration): void {
   for (const [key, value] of Object.entries(source)) {
     if (!Object.prototype.hasOwnProperty.call(target, key)) {
